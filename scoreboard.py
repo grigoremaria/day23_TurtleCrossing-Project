cac21f3d743b04fbf73e94636a5cc89fbde81db5
grigoremaria <mariaagrigore30@gmail.com>
2025-02-17 13:58:07 +0200

    day 22 - turtle crossing project

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 260)  # Poziționare scor
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
